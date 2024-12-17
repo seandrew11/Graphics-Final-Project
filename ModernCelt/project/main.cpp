f4b4dbb21371dfd0c534fc9d59a70c91a129c118
seandrew11 <73836872+seandrew11@users.noreply.github.com>
2024-12-17 09:31:39 +0000

    added camera movement

--- ModernCelt/project/main.cpp
+++ ModernCelt/project/main.cpp
@@ -37,7 +37,47 @@ GLuint LoadTerrainTexture(const char* texture_file_path) {
     return texture;
 }
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_R && action == GLFW_PRESS)
+    {
+        viewAzimuth = 0.f;
+        viewPolar = 0.f;
+        eye_center.y = viewDistance * cos(viewPolar);
+        eye_center.x = viewDistance * cos(viewAzimuth);
+        eye_center.z = viewDistance * sin(viewAzimuth);
+        std::cout << "Reset." << std::endl;
+    }
+
+    if (key == GLFW_KEY_UP && (action == GLFW_REPEAT || action == GLFW_PRESS))
+    {
+        viewPolar -= 0.1f;
+        eye_center.y = viewDistance * cos(viewPolar);
+    }
+
+    if (key == GLFW_KEY_DOWN && (action == GLFW_REPEAT || action == GLFW_PRESS))
+    {
+        viewPolar += 0.1f;
+        eye_center.y = viewDistance * cos(viewPolar);
+    }
+
+    if (key == GLFW_KEY_LEFT && (action == GLFW_REPEAT || action == GLFW_PRESS))
+    {
+        viewAzimuth -= 0.1f;
+        eye_center.x = viewDistance * cos(viewAzimuth);
+        eye_center.z = viewDistance * sin(viewAzimuth);
+    }
+
+    if (key == GLFW_KEY_RIGHT && (action == GLFW_REPEAT || action == GLFW_PRESS))
+    {
+        viewAzimuth += 0.1f;
+        eye_center.x = viewDistance * cos(viewAzimuth);
+        eye_center.z = viewDistance * sin(viewAzimuth);
+    }
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main(void) {
     // Initialize GLFW
@@ -120,8 +160,4 @@ int main(void) {
     return 0;
 }
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-}
+
